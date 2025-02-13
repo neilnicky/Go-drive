512846d9a0001b5340806468e624a46bacd257fa
Neil Nicholas <neilnicholaskanat@gmail.com>
2025-02-13 10:04:47 +0530

    created page component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,9 @@
+import Component from "~/components/Dash";
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <h1>Hello World</h1>
+    <main className="text-white">
+     <Component/>
     </main>
   );
 }
